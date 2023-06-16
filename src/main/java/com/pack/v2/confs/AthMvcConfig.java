6871b34317e4156f0d72928242d7a7654a723255
Ashim Eminov <ashim030312@gmail.com>
2023-06-16 21:06:14 +0600

    Added WebSecurityConfig

---
+++ src/main/java/com/pack/v2/confs/AthMvcConfig.java
@@ -0,0 +1,14 @@
+package com.pack.v2.confs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AthMvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
