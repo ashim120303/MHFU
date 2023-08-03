dd3af728111d73ae2429af87c8a7143bd1531835
ashim120303 <ashim030312@gmail.com>
2023-08-03 14:20:46 +0600

    Fixed the modal window.

--- src/main/java/com/pack/v2/contollers/SettingsController.java
+++ src/main/java/com/pack/v2/contollers/SettingsController.java
@@ -2,6 +2,7 @@ package com.pack.v2.contollers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SettingsController {
