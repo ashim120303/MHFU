6360501f7668c3957c3d84b1969e36372c6fd121
ashim120303 <ashim030312@gmail.com>
2023-09-11 17:45:13 +0600

    Completed responsiveness.

--- src/main/resources/static/js/settings.js
+++ src/main/resources/static/js/settings.js
@@ -3,7 +3,7 @@ let modal = document.getElementById("myModal");
 let btn = document.getElementById("myBtn");
 let span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
-  modal.style.display = "block";
+  modal.style.display = "flex";
 }
 span.onclick = function() {
   modal.style.display = "none";
